ce77836ee44e8566fbc36b392141108705fbd58f
dcars18 <deca222@g.uky.edu>
2017-03-29 11:12:27 -0400

    Okay It works now...

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express')
 var mailer = require('./mailer')
 var app = express()
 
+app.use(require('body-parser').json({ extended: true }));
+
 app.post('/SARAEmail', function (req, res) {
 	//res.send('Hello World!')
-	console.log(req.email);
-	res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	console.log(req.body.name);
+	//res.header("Access-Control-Allow-Origin", "*");
+    //res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	
 	res.send("Email would be sent...")
 	//res.sendStatus(200);
